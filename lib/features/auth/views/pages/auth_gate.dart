99561b59eacd900e1d55eae788b08284e3d553b7
Abdelrhman Mohamed <abdelrhmanhanafi2@gmail.com>
2026-01-03 04:31:12 +0200

    update pupspec

--- lib/features/auth/views/pages/auth_gate.dart
+++ lib/features/auth/views/pages/auth_gate.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_mate/features/auth/auth_cubit/auth_cubit.dart';
 import 'package:social_mate/features/auth/views/pages/auth_page.dart';
-import 'package:social_mate/features/home/views/pages/home_page.dart';
 import 'package:social_mate/root.dart';
 
 class AuthGate extends StatelessWidget {
